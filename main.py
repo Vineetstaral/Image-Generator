0cf7a57a599f795d11a0cb4b17e8e2aa241f5a48
Vineetstaral <114866729+Vineetstaral@users.noreply.github.com>
2025-03-20 14:03:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+import io
+from PIL import Image
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("HUGGINGFACE_API_KEY")
+
+API_URL = "https://api-inference.huggingface.co/models/alvdansen/littletinies"
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+st.title("AI Image Generator")
+st.write("Enter a description to generate an image:")
+
+
+description = st.text_input("Description")
+
+if st.button("Generate Image"):
+    if description:
+        with st.spinner('Generating image...'):
+            image_bytes = query({"inputs": description})
+            image = Image.open(io.BytesIO(image_bytes))
+            st.image(image, caption=description)
+    else:
+        st.warning("Please enter a description.")
+
+st.markdown("---")
+st.markdown("This is AI generated Image")
